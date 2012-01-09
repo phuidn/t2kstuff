d36e7e1b3353af20da9732debffa71565384cb6f
dean <dean.birch0@gmail.com>
2012-01-09 13:19:59 +0000

    should have modified cuts.C (both) to take UInt_t Detectors, not GlobalPID as before

--- recon/treetest/v0r0/app/cuts.h
+++ recon/treetest/v0r0/app/cuts.h
@@ -1,10 +1,1 @@
-//cut functions
-int inFGD1( TLorentzVector *FrontPosition );
-int inFGD2( TLorentzVector *FrontPosition );
-int noPODactivity(ND::TGlobalReconModule::TGlobalPID *gTrack );
-int noTPC1(ND::TGlobalReconModule::TGlobalPID *gTrack );
-int inTPC2(ND::TGlobalReconModule::TGlobalPID *gTrack );
-int inTPC3(ND::TGlobalReconModule::TGlobalPID *gTrack );
-int inBeamTime(TLorentzVector *FrontPosition, double beamTimeCut = 100.);
-int inTimeBunch(TLorentzVector *FrontPosition, double beamTimeCut = 100.);
-
+../../../cuts/v0r0/app/cuts.h
