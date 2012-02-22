b06cf9ef0b47f9144390c001b07e1365e2a8e4ad
dean <dean.birch0@gmail.com>
2012-02-22 12:03:17 +0000

    Added new C script, sorry its late!

--- recon/cuts/v0r0/app/energycorrectnopython.C
+++ recon/cuts/v0r0/app/energycorrectnopython.C
@@ -1,3 +1,8 @@
+//Notes: is matrix right way round?
+//		are bounds and energies in corect units
+//
+
+
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
@@ -26,6 +31,52 @@ int main(int argc, char* argv[])
 	SetupROOT();		
 
 	//Input section
+	//	Reads from kinetic energy list to make matrix and data vectors
+	ifstream file("kinetic-out2.txt");
+	string line;
+	vector<double> truthv,reconv;
+	int nbins=5;
+	double bounds[6]={0.,0.13,0.25,0.40,0.90,17.};
+	for(int i=0;i<6;i++) bounds[i] = bounds[i]*1000000./(2.*938.);
+	for(int i=0;i<6;i++) cout <<bounds[i] <<endl;
+	int startpos,endpos;
+	int len=-1;
+	while(file.good())
+	{
+		getline(file,line,',');
+		reconv.push_back(atof(line.c_str()));
+		getline(file,line);
+		truthv.push_back(atof(line.c_str()));
+		len++;
+	}
+	for(int i=0;i<len;i++)
+	{//normalise to correct units
+		truthv[i] = truthv[i]/(2.*938.);
+		reconv[i] = reconv[i]/(2.*938.);
+	}
+	cout << "Vectors made, "<<len<<" lines long."<<endl;
+
+	//Populate Histograms for unfolding
+	TH1D *reconhist = new TH1D("reconhist","Title",nbins,bounds);
+	TH1D *truthhist = new TH1D("truthhist","Title",nbins,bounds);
+	TH2D *matrixhist= new TH2D("matrixhist","Titl3",nbins,bounds,nbins,bounds);
+	TH1D *datahist = new TH1D("datahist","Title",nbins,bounds);
+	TH1D *truedatahist = new TH1D("truedatahist","Title",nbins,bounds);
+	
+	cout <<"Looping over vectors to make TH1Ds..."<<endl;
+	for(int i=0;i<len;i++)
+	{
+		reconhist->Fill(reconv[i]);
+		truthhist->Fill(truthv[i]);
+		matrixhist->Fill(reconv[i],truthv[i]);
+	}
+	
+	truthhist->Draw();
+	App->Run();
+
+	return 0;
+
+/*	//Input section (old)
 	ifstream infile("energyhists.txt");
 	unsigned int nbins(0), felement(0);
 	string line;
@@ -44,8 +95,9 @@ int main(int argc, char* argv[])
 		parseline(nbins,matrix[i],&infile);
 	parseline(nbins, data, &infile); //lines 11 data
 	parseline(nbins, truedata, &infile);//line 12 true values of data
+*/
 
-	//Populate Histograms for unfolding
+/*	//Populate Histograms for unfolding
 	TH1D *reconhist = new TH1D("reconhist","Title",nbins,bounds);
 	populateTH1D(nbins,bounds,recon,reconhist);
 	TH1D *truthhist = new TH1D("truthhist","Title",nbins,bounds);
@@ -58,7 +110,7 @@ int main(int argc, char* argv[])
 	populateTH1D(nbins,bounds,truedata,truedatahist);
 
 	//Unfolding section
-	//****Using the same recon data atm!! ***** CHANGE!!*****//
+	//Using the same recon data atm!! ***** CHANGE!!
 	TSVDUnfold *unf = new TSVDUnfold(datahist,reconhist,truthhist,matrixhist);
 	TH1D *unfresult = unf->Unfold(1);
 	for(int k(0); k<nbins+2;k++)
@@ -71,9 +123,10 @@ int main(int argc, char* argv[])
 	cout << endl;
 //	reconhist->Draw("same");
 	App->Run();
-	
+*/	
 	return 0;	
 }
+
 
 int populateTH1D(int nbins,double *bounds, double *bins,TH1D *graph)
 {
